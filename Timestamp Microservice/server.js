a2713f2755d8223279019f60bd27f9ca8bfd124a
Aditya Verma <adi004.zz@gmail.com>
2018-08-06 00:25:29 +0530

    feat: Added html

--- Timestamp Microservice/server.js
+++ Timestamp Microservice/server.js
@@ -8,19 +8,25 @@ const PORT = process.env.PORT || 1337;
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+    res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
 app.get("/api/timestamp/:date_string?", (req, res) => {
-  let dateString = req.params.date_string;
-  const date = dateString ? new Date(dateString) : new Date();
-  res.send(
-    date.toString() === "Invalid Date"
-      ? { error: "Invalid Date" }
-      : { unix: date.getTime(), utc: date.toUTCString() }
-  );
+    let dateString = req.params.date_string;
+    dateString = (parseInt(dateString) === +dateString) ? +dateString : dateString;
+    const date = dateString ? new Date(dateString) : new Date();
+    res.send(
+        date.toString() === "Invalid Date" ?
+        {
+            error: "Invalid Date"
+        } :
+        {
+            unix: date.getTime(),
+            utc: date.toUTCString()
+        }
+    );
 });
 
 app.listen(PORT, () =>
-  console.log(`The server is running on the Port: ${PORT}`)
+    console.log(`The server is running on the Port: ${PORT}`)
 );
