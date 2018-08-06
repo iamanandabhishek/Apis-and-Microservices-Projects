ca334d0ee90c3a5715421e0567dbaa209169d660
Aditya Verma <adi004.zz@gmail.com>
2018-08-06 18:38:17 +0530

    feat: added html and style files

--- Request Header Parser Microservice/server.js
+++ Request Header Parser Microservice/server.js
@@ -5,31 +5,25 @@ const app = express();
 
 const PORT = process.env.PORT || 1337;
 
-
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.send("hello world");
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
+
 
 app.get("/api/whoami", (req, res) => {
-    const ip = (req.headers['x-forwarded-for']  || req.connection.remoteAddress || req.ip).split(",")[0].trim();
-    const software = req.headers["user-agent"];
-    const language = req.headers["accept-language"];
-   
-    res.send({ipaddress: ip,  language: language, software: software});
+  const ip = (
+    req.headers["x-forwarded-for"] ||
+    req.connection.remoteAddress ||
+    req.ip
+  )
+    .split(",")[0]
+    .trim();
+  const software = req.headers["user-agent"];
+  const language = req.headers["accept-language"];
+
+  res.send({ ipaddress: ip, language: language, software: software });
 });
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(PORT, () => console.log(`Server is running on the Port ${PORT}`));
