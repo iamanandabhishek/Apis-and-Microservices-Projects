24d5cf94cd36c2769d4a99f1fec17939c8a146ae
Aditya Verma <adi004.zz@gmail.com>
2018-08-10 00:53:03 +0530

    feat: added server logic, completed the project

--- File Metadata Microservice/public/script.js
+++ File Metadata Microservice/public/script.js
@@ -1,6 +1,19 @@
 document.querySelector("form").addEventListener("submit", function(e){
     e.preventDefault();
+    document.querySelector(".preloader").classList.remove("hide");
     const file = e.target.querySelector("input");
+    const formData = new FormData();
+    formData.append("file", file.files[0]);
+    
     console.log(file.value, file.files[0]);
+
+    axios.post("/api/fileanalyse", formData, {
+        headers: {"Content-Type" : "multipart/form-data"}     
+    }).then(res => {
+        console.log(res);
+        window.location.href = res.data.redirect;
+    }).catch(err => {
+        console.log(err);
+    })
     
 });
--- File Metadata Microservice/server/server.js
+++ File Metadata Microservice/server/server.js
@@ -1,17 +1,44 @@
 const express = require("express");
 const path = require("path");
+const multer = require("multer");
 
 const app = express();
 
 const PORT = process.env.PORT || 1337;
 
-
+const upload = multer({
+    dest: "uploads/"
+});
 app.use(express.static("public"));
+
+
+let dataToSend;
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../views", "index.html"));
 });
 
+app.get("/api/fileanalyse", (req, res) => {
+    if (dataToSend) {
+        res.send(dataToSend);
+    } else {
+        res.send("Please choose a file");
+    }
+
+})
+
+//upload.single(filename) should be same as in the form, or you'll waste 3hrs ripping your hair apart like me.
+app.post("/api/fileanalyse", upload.single("file"), function (req, res) {
+    if (req.file) {
+        dataToSend = {
+            name: req.file.originalname,
+            type: req.file.mimetype,
+            size: req.file.size
+        }
+    }
+    res.send({
+        redirect: "/api/fileanalyse"
+    });
+});
 
 app.listen(PORT, () => console.log(`Server is running on the PORT ${PORT}`));
-
